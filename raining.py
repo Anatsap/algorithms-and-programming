296185bcef148bf902f20c03664c62cd3dbf6ad0
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2024-09-22 17:31:19 +0300

    delete files from lab_1

--- raining.py
+++
@@ -1,3 +0,0 @@
-is_raining = False
-is_snowing = False
-print(not is_raining, not is_snowing)
--- student.py
+++
@@ -1,3 +0,0 @@
-is_student = False
-is_teacher = True
-print(is_student and is_teacher, is_student or is_teacher, not is_student, not is_teacher)
