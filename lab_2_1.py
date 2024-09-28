1777bf10e07e44d140ed0994cc45e7abcc36fc24
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2024-09-28 15:22:16 +0300

    add rounding

--- lab_2_1.py
+++ lab_2_1.py
@@ -5,15 +5,17 @@ a = 1.5
 b = 3.5
 h = 0.05
 x = a 
-while x <= b + 1e-8:
+
+while x <= b :
 
     if x < 2.2:
         result = log10(log(x)+log2(x))
     elif x >= 2.2 and x < 3:
-        result = (cos(x)**2+(cos(x)/sin(x)))**(1/4)
-    elif x >= 3:
+        result = (cos(x)**2)+(cos(x)/sin(x))**(1/4)
+    elif x >= 3.5:
         result = atan(1/x)
-
-    print(result)
+    print(f"x = {x:.2f}")
     x += h
+    x = round(x, 2s)
+    print((x))
     
